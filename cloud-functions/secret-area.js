71953a75d1dd7173bf310bafbc8659f8a2680f75
Michael Martin <mictin3@gmail.com>
2020-03-20 15:59:50 -0600

    Updated cloud function to check password again but fixed error

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -12,13 +12,14 @@ exports.handler = function(event, context, callback) {
     body = {};
   }
 
-  if (body.password === 'javascript') {
+  if (body.password == 'javascript') {
     callback(null, {
       statusCode: 200,
       body: secretContent
-    })
+    });
   } else {
     callback(null, {
       statusCode: 401
-    })
+    });
   }
+};
